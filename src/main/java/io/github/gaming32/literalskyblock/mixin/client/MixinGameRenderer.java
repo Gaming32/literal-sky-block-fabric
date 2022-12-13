a53e6b856d758b715cde36b69c91d521ee1d009e
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2022-12-13 00:39:47 -0600

    Improvements and fixes

--- src/main/java/io/github/gaming32/literalskyblock/mixin/client/MixinGameRenderer.java
+++ src/main/java/io/github/gaming32/literalskyblock/mixin/client/MixinGameRenderer.java
@@ -2,12 +2,10 @@ package io.github.gaming32.literalskyblock.mixin.client;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.datafixers.util.Pair;
-import com.mojang.math.Matrix3f;
 import com.mojang.math.Matrix4f;
 import io.github.gaming32.literalskyblock.client.LSBClient;
 import io.github.gaming32.literalskyblock.forge.ForgeHooksClient;
 import io.github.gaming32.literalskyblock.forge.RegisterShadersEvent;
-import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.ShaderInstance;
@@ -18,9 +16,9 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.io.IOException;
 import java.util.List;
@@ -32,6 +30,8 @@ public class MixinGameRenderer {
 
     @Unique
     private ResourceManager forge$resourceManager;
+    @Unique
+    private Matrix4f forge$renderMatrix;
 
     @Inject(method = "reloadShaders", at = @At("HEAD"))
     private void storeResourceManager(ResourceManager manager, CallbackInfo ci) {
@@ -55,17 +55,21 @@ public class MixinGameRenderer {
         return true;
     }
 
+    @ModifyVariable(method = "renderLevel", at = @At("STORE"), ordinal = 0)
+    private Matrix4f storeRenderMatrix(Matrix4f matrix4f) {
+        return forge$renderMatrix = matrix4f;
+    }
+
     @Inject(
         method = "renderLevel",
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/client/renderer/LevelRenderer;renderLevel(Lcom/mojang/blaze3d/vertex/PoseStack;FJZLnet/minecraft/client/Camera;Lnet/minecraft/client/renderer/GameRenderer;Lnet/minecraft/client/renderer/LightTexture;Lcom/mojang/math/Matrix4f;)V",
             shift = At.Shift.AFTER
-        ),
-        locals = LocalCapture.CAPTURE_FAILHARD
+        )
     )
-    private void implementRenderLast(float tickDelta, long limitTime, PoseStack matrix, CallbackInfo ci, boolean bl, Camera camera, PoseStack poseStack, double d, float f, Matrix4f matrix4f, Matrix3f matrix3f) {
+    private void implementRenderLast(float tickDelta, long limitTime, PoseStack matrix, CallbackInfo ci) {
         minecraft.getProfiler().popPush("forge_render_last");
-        ForgeHooksClient.dispatchRenderLast(minecraft.levelRenderer, matrix, tickDelta, matrix4f, limitTime);
+        ForgeHooksClient.dispatchRenderLast(minecraft.levelRenderer, matrix, tickDelta, forge$renderMatrix, limitTime);
     }
 }
